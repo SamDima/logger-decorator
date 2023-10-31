9f1ae94393bad0db02f2a270f8a8dd750912b87f
Dmitriy.Ignatyev <dmitriy.ignatyev@quantori.com>
2023-10-31 13:56:55 -0300

    increase version

--- logger_decorator/setup.py
+++ logger_decorator/setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION="3.2.0"
+VERSION="3.2.1"
 
 setup(
     name="logger-decorator",
